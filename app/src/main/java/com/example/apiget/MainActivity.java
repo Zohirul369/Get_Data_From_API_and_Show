439ece98675453cfc8d6d1887458e2d0706f605a
Zohirul Islam <zohirul369@gmail.com>
2023-12-26 17:38:09 +0600

    Update and add table header

--- app/src/main/java/com/example/apiget/MainActivity.java
+++ app/src/main/java/com/example/apiget/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.apiget;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,49 +23,77 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        @NonNull ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         String url = "https://uatezone.octimsbd.com/api/company";
 
-        ArrayList<String> NameArrayList = new ArrayList<>();
+        TableLayout tableLayout = findViewById(R.id.tableLayout);
+
+        // Add the title row
+        TableRow titleRow = new TableRow(MainActivity.this);
+        titleRow.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+
+        // Add a single cell with the title text
+        TextView titleTextView = new TextView(MainActivity.this);
+        titleTextView.setText("All Company Information");
+        titleTextView.setGravity(Gravity.CENTER);
+        titleTextView.setLayoutParams(new TableRow.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, 4f));
+        titleRow.addView(titleTextView);
+
+        // Add the title row to the table
+        tableLayout.addView(titleRow);
 
         StringRequest request = new StringRequest(url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-
                 try {
                     JSONArray jsonArray = new JSONArray(response);
-                    for (int i = 0 ; i< jsonArray.length(); i++){
+
+                    // Add table headers if needed
+                    TableRow headerRow = new TableRow(MainActivity.this);
+                    headerRow.setLayoutParams(new TableLayout.LayoutParams(
+                            TableLayout.LayoutParams.MATCH_PARENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT));
+
+                    // Add header cells
+                    addTableCell(headerRow, "Code");
+                    addTableCell(headerRow, "Name");
+
+                    // Add header row to the table
+                    tableLayout.addView(headerRow);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                        String Code = jsonObject.getString("Code");
-                        String CompanyId = jsonObject.getString("CompanyId");
-                        String ConcernId = jsonObject.getString("ConcernId");
-                        String Name = jsonObject.getString("Name");
-                        NameArrayList.add(Code);
-                        NameArrayList.add(CompanyId);
-                        NameArrayList.add(ConcernId);
-                        NameArrayList.add(Name);
+                        String code = jsonObject.getString("Code");
+                        String name = jsonObject.getString("Name");
+
+                        // Create a new row for each item
+                        TableRow row = new TableRow(MainActivity.this);
+                        row.setLayoutParams(new TableLayout.LayoutParams(
+                                TableLayout.LayoutParams.MATCH_PARENT,
+                                TableLayout.LayoutParams.WRAP_CONTENT));
+
+                        // Add cells to the row
+                        addTableCell(row, code);
+                        addTableCell(row, name);
+
+                        // Add the row to the table
+                        tableLayout.addView(row);
                     }
-
-                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this,
-                            android.R.layout.simple_expandable_list_item_1, NameArrayList);
-                    binding.listview.setAdapter(arrayAdapter);
-
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
-
             }
         }, new Response.ErrorListener() {
             @Override
@@ -71,5 +104,15 @@ public class MainActivity extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(request);
+    }
+
+    private void addTableCell(TableRow row, String text) {
+        TextView textView = new TextView(MainActivity.this);
+        textView.setText(text);
+        textView.setGravity(Gravity.CENTER);
+        textView.setLayoutParams(new TableRow.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+        row.addView(textView);
     }
 }
